26086fda0097dddeb82c94cdf958087ab8f86165
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-02-06 19:57:14 +0700

    Time: 122 ms (77.52%), Space: 51.3 MB (9.52%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int Search(int[] nums, int target) 
+    {
+        int middle = 0;
+        int begin = 0;
+        int end = nums.Length -1;
+
+        while (begin <= end)
+        {
+            middle = (begin + end) / 2; 
+
+            if (nums[middle] > target)
+            {
+                end = middle - 1;
+            }
+
+            else if (nums[middle] < target)
+            {
+                begin = middle + 1;
+            }
+
+            else return middle;
+        }
+
+        return -1;
+    }
+}
