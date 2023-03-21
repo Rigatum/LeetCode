37513d78577a3baf358ca4c4d776bb79a286235f
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-03-21 20:27:12 +0700

    Time: 26 ms (49.57%), Space: 28.1 MB (5.59%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cs
@@ -0,0 +1,20 @@
+public class Solution 
+{
+    public int ClimbStairs(int n) 
+    {
+        return fib(n, new int[10000]);
+    }
+    
+    int fib(int n, int[] memo)
+    {
+        if(n < 0)
+            return 0;
+        if(n == 0)
+            return 1;
+        
+        if (memo[n] == 0)
+            memo[n] = fib(n-1, memo) + fib(n-2, memo);
+            
+        return memo[n];
+    }
+}
