e327e8524fe0fbc55a6f0b075d5f8c6e44517f92
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-03-26 15:06:51 +0700

    Time: 223 ms (100.00%), Space: 45.3 MB (100.00%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cs
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cs
@@ -2,47 +2,53 @@ public class Solution
 {
     public bool PrimeSubOperation(int[] nums) 
     {
-        int[] prime_nums = new int[] { 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,
-            73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,
-            179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,
-            283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,
-            419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,
-            547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,
-            661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,
-            811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,
-            947,953,967,971,977,983,991,997,1009 };
-        int p = 0;
-        for (int i = 0; i < nums.Length; i++){
-            int left = 0;
-            int right = prime_nums.Length - 1;
-            while (left <= right){
-                int mid = (left + right) / 2;
-                if (prime_nums[mid] > nums[i])
-                    right = mid - 1;
-                else if (prime_nums[mid] < nums[i])
-                    left = mid + 1;
-                else{
-                    p = mid - 1;
-                    break;
+                List<int> prime_nums = new List<int>();
+                for (int i = 2; i < 1000; i++)
+                {
+                    if(isPrime(i))
+                        prime_nums.Add(i);
                 }
-                p = left - 1;
-            }
-            if (i > 0){
-                while (p > 0 && nums[i - 1] >= nums[i] - prime_nums[p] && i >= 0  && nums[i] > prime_nums[p]){
-                    p--;
+                bool isPrime(int n)
+                {
+                    for (int i = 2; i < n; i++)
+                    {
+                        if (n % i == 0)
+                            return false;
+                    }
+                    return true;
                 }
-                if (p >= 0 && nums[i - 1] < nums[i] - prime_nums[p]){
-                    nums[i] = nums[i] - prime_nums[p];
+                int p = 0;
+                for (int i = 0; i < nums.Length; i++){
+                    int left = 0;
+                    int right = prime_nums.Count - 1;
+                    while (left <= right){
+                        int mid = (left + right) / 2;
+                        if (prime_nums[mid] > nums[i])
+                            right = mid - 1;
+                        else if (prime_nums[mid] < nums[i])
+                            left = mid + 1;
+                        else{
+                            p = mid - 1;
+                            break;
+                        }
+                        p = left - 1;
+                    }
+                    if (i > 0){
+                        while (p > 0 && nums[i - 1] >= nums[i] - prime_nums[p] && i >= 0  && nums[i] > prime_nums[p]){
+                            p--;
+                        }
+                        if (p >= 0 && nums[i - 1] < nums[i] - prime_nums[p]){
+                            nums[i] = nums[i] - prime_nums[p];
+                        }
+                    }
+                    else if (p >= 0 && nums[0] > nums[0] - prime_nums[p]){
+                        nums[0] = nums[0] - prime_nums[p];
+                    }
+                    if (i > 0 && nums[i - 1] < nums[i])
+                        continue;
+                    else if (i > 0)
+                        return false;
                 }
-            }
-            else if (p >= 0 && nums[0] > nums[0] - prime_nums[p]){
-                nums[0] = nums[0] - prime_nums[p];
-            }
-            if (i > 0 && nums[i - 1] < nums[i])
-                continue;
-            else if (i > 0)
-                return false;
-        }
-        return true;
+                return true;
     }
 }
