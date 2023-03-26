39a74722d2d56d2caca00423bf7250fd71ffcc8a
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-03-26 12:33:41 +0700

    Time: 127 ms (100.00%), Space: 44.4 MB (100.00%) - LeetHub

---
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cs
@@ -0,0 +1,95 @@
+public class Solution 
+{
+    public bool PrimeSubOperation(int[] nums) 
+    {
+        int[] prime_nums = new int[] { 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,
+            73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,
+            179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,
+            283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,
+            419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,
+            547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,
+            661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,
+            811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,
+            947,953,967,971,977,983,991,997,1009 };
+
+        int p = 0;
+
+        int left2 = 0;
+        int right2 = prime_nums.Length - 1;
+
+        while (left2 <= right2)
+        {
+            int mid = (left2 + right2) / 2;
+
+            if (prime_nums[mid] > nums[0])
+                right2 = mid - 1;
+
+            else if (prime_nums[mid] < nums[0])
+                left2 = mid + 1;
+
+            else
+            {
+                if (prime_nums[mid] != nums[0])
+                    p++;
+                p = mid - 1;
+                break;
+            }
+            p = left2 - 1;
+        }
+
+        if (p >= 0 && nums[0] > nums[0] - prime_nums[p])
+        {
+            nums[0] = nums[0] - prime_nums[p];
+        }
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            int left = 0;
+            int right = prime_nums.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (prime_nums[mid] > nums[i])
+                    right = mid - 1;
+
+                else if (prime_nums[mid] < nums[i])
+                    left = mid + 1;
+
+                else
+                {
+                    p = mid - 1;
+                    break;
+                }
+                p = left - 1;
+            }
+
+            while (p > 0 && nums[i - 1] >= nums[i] - prime_nums[p] && i >= 0  && nums[i] > prime_nums[p])
+            {
+                p--;
+            }
+
+            if (p >= 0 && nums[i - 1] < nums[i] - prime_nums[p])
+            {
+                nums[i] = nums[i] - prime_nums[p];
+            }
+        }
+
+        bool IsSorted(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] < nums[i + 1])
+                {
+                    continue;
+                }
+                else
+                    return false;
+            }
+            return true;
+        }
+
+        return IsSorted(nums);
+    }
+}
