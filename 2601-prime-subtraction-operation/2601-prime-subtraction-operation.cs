3bc42344a9c0badef602afe100e2cd55394f9374
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-03-26 15:12:16 +0700

    Time: 237 ms (100.00%), Space: 45.8 MB (100.00%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cs
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cs
@@ -18,30 +18,37 @@ public class Solution
                     return true;
                 }
                 int p = 0;
-                for (int i = 0; i < nums.Length; i++){
+                for (int i = 0; i < nums.Length; i++)
+                {
                     int left = 0;
                     int right = prime_nums.Count - 1;
-                    while (left <= right){
+                    while (left <= right)
+                    {
                         int mid = (left + right) / 2;
                         if (prime_nums[mid] > nums[i])
                             right = mid - 1;
                         else if (prime_nums[mid] < nums[i])
                             left = mid + 1;
-                        else{
+                        else
+                        {
                             p = mid - 1;
                             break;
                         }
                         p = left - 1;
                     }
-                    if (i > 0){
-                        while (p > 0 && nums[i - 1] >= nums[i] - prime_nums[p] && i >= 0  && nums[i] > prime_nums[p]){
+                    if (i > 0)
+                    {
+                        while (p > 0 && nums[i - 1] >= nums[i] - prime_nums[p] && i >= 0  && nums[i] > prime_nums[p])
+                        {
                             p--;
                         }
-                        if (p >= 0 && nums[i - 1] < nums[i] - prime_nums[p]){
+                        if (p >= 0 && nums[i - 1] < nums[i] - prime_nums[p])
+                        {
                             nums[i] = nums[i] - prime_nums[p];
                         }
                     }
-                    else if (p >= 0 && nums[0] > nums[0] - prime_nums[p]){
+                    else if (p >= 0 && nums[0] > nums[0] - prime_nums[p])
+                    {
                         nums[0] = nums[0] - prime_nums[p];
                     }
                     if (i > 0 && nums[i - 1] < nums[i])
