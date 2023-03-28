8ff99028b4fcca22c082153fef832a86f005f456
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-03-28 18:29:12 +0700

    Time: 159 ms (38.43%), Space: 45.6 MB (85.15%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cs
+++ 0735-asteroid-collision/0735-asteroid-collision.cs
@@ -4,43 +4,17 @@ public class Solution
     {
         Stack<int> stack = new Stack<int>();
         stack.Push(asteroids[0]);
-        int j = 1;
+
         for(int i = 1; i < asteroids.Length; i++)
         {
-            if(stack.TryPeek(out int previous) && (previous > 0  && asteroids[i] < 0))
+  collison: if(stack.TryPeek(out int previous) && (previous > 0  && asteroids[i] < 0))
             {
                 if (Math.Abs(previous) > Math.Abs(asteroids[i]))
                     continue;
                 else if (Math.Abs(previous) < Math.Abs(asteroids[i]))
                 {
                     stack.Pop();
-                    stack.Push(asteroids[i]);
-
-                    while(j < stack.Count)
-                    {
-                        if ((stack.Peek() < 0 && stack.ElementAt(j) > 0))
-                        {
-                            if (Math.Abs(stack.Peek()) > Math.Abs(stack.ElementAt(j)))
-                            {
-                                stack.Pop();
-                                stack.Pop();
-                                stack.Push(asteroids[i]);
-                            }
-                            else if (Math.Abs(stack.Peek()) < Math.Abs(stack.ElementAt(j)))
-                            {
-                                stack.Pop();
-                                break;
-                            }
-                            else
-                            {
-                                stack.Pop();
-                                stack.Pop();
-                                break;
-                            }
-                        }
-                        else
-                            break;
-                    }
+                    goto collison;
                 }
                 else
                     stack.Pop();
@@ -48,7 +22,7 @@ public class Solution
             else
                 stack.Push(asteroids[i]);
         }
-        
-        return stack.Reverse().ToArray();;
+
+        return stack.Reverse().ToArray();
     }
 }
