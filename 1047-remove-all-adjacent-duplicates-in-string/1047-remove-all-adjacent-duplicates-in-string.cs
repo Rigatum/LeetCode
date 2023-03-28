129f1c5cf5c15ca20119a7aac5965cc29cbdd99c
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-03-28 13:02:03 +0700

    Time: 101 ms (47.76%), Space: 46.3 MB (68.66%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cs
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cs
@@ -3,24 +3,25 @@ public class Solution
     public string RemoveDuplicates(string s) 
     {
         Stack<char> stack = new Stack<char>();
-        StringBuilder ans = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         char previous;
         stack.Push(s[0]);
+        sb.Append(s[0]);
 
         for (int i = 1; i < s.Length; i++)
         {
             if (stack.TryPeek(out previous) && s[i] == previous)
+            {
                 stack.Pop();
+                sb.Remove(sb.Length - 1, 1);
+            }
             else
             {
                 stack.Push(s[i]);
+                sb.Append(s[i]);
             }
         }
-        foreach (var item in stack.Reverse())
-        {
-            ans.Append(item);
-        }
 
-        return ans.ToString();
+        return sb.ToString();
     }
 }
