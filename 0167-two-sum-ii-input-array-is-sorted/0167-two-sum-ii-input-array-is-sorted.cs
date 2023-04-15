16d03cb6fa737d6f15d802bfdd7433baeeccd33a
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-04-15 16:09:04 +0700

    Time: 155 ms (57.76%), Space: 46.2 MB (59.90%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cs
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cs
@@ -2,39 +2,18 @@ public class Solution
 {
     public int[] TwoSum(int[] numbers, int target) 
     {
-        for (int i = 0; i < numbers.Length; i++)
+        int left = 0;
+        int right = numbers.Length - 1;
+
+        while(left < right)
         {
-            int ans = binarySearch(target - numbers[i], numbers, i);
-            if (ans > -1)
-            {
-                return new int[] {++i, ++ans};
-            }
+            if (numbers[left] + numbers[right] == target)
+                return new int[] {++left, ++right};
+            else if (numbers[left] + numbers[right] < target)
+                left++;
+            else 
+                right--;
         }
         return null;
-
-        int binarySearch(int target, int[] numbers, int index)
-        {
-            int left = 0;
-            int right = numbers.Length - 1;
-
-            while(left <= right)
-            {
-                int middle = (left + right) / 2;
-                if (target > numbers[middle])
-                    left = middle + 1;
-                else if (target < numbers[middle])
-                    right = middle - 1;
-                else if (target == numbers[middle] && index == middle)
-                {
-                    if (numbers[middle + 1] == target)
-                        return middle + 1;
-                    else
-                        return middle - 1;
-                }
-                else
-                    return middle;
-            }
-            return -1;
-        }
     }
 }
