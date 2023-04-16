477125ec412865f36775521e365f9eae499212a8
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-04-16 08:03:00 +0700

    Time: 84 ms (62.22%), Space: 40.7 MB (60.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cs
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -4,38 +4,26 @@ public class Solution
 {
     public bool IsPalindrome(string s) 
     {
-            if(s.Length == 1)
-                return true;
-            s = s.ToLower();
-            int left = 0;
-            int right = s.Length - 1;
-            int k = 0;
-            while (left < right)
+        if(s.Length == 1)
+            return true;
+        s = s.ToLower();
+        int left = 0;
+        int right = s.Length - 1;
+        int k = 0;
+        while (left < right)
+        {
+            if (!Char.IsLetterOrDigit(s[left]))
+                left++;
+            else if (!Char.IsLetterOrDigit(s[right]))
+                right--;
+            else
             {
-                while (left < s.Length && !Char.IsLetterOrDigit(s[left]))
-                    left++;
-                while (right > 0 && !Char.IsLetterOrDigit(s[right]))
-                    right--;
-                if(left < s.Length && right >= 0 && Char.IsLetterOrDigit(s[left]) && Char.IsLetterOrDigit(s[right]) && s[right] == s[left])
-                {
-                    left++;
-                    right--;
-                    k++;
-                }
-                else if (left < s.Length && right >= 0 && s[right] != s[left])
+                if(s[left] != s[right])
                     return false;
+                left++;
+                right--;
             }
-            int q = 0;
-            foreach (var item in s)
-            {
-                if(Char.IsLetterOrDigit(item))
-                    q++;
-            }
-            if(q == 0)
-                return true;
-            if (k > 0)
-                return true;
-            else 
-                return false;
+        }
+        return true;
     }
 }
