e84ded478bd827459934c74aa99c4f29c2f94206
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-04-17 16:42:46 +0700

    Time: 184 ms (62.94%), Space: 51.3 MB (8.88%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cs
@@ -0,0 +1,21 @@
+public class Solution 
+{
+    public int MaxArea(int[] height) 
+    {
+        int left = 0;
+        int right = height.Length - 1;
+        int maxConteiner = -1; 
+        int shortLine = 0;
+
+        while (right > 0 && left < height.Length)
+        {
+            shortLine = height[left] <= height[right] ? height[left] : height[right];
+            maxConteiner = (right - left) * shortLine <= maxConteiner ? maxConteiner : (right - left) * shortLine;
+            if (height[right] == shortLine)
+                right--;
+            else
+                left++;
+        }
+        return maxConteiner;
+    }
+}
