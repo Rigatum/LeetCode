030cb78cb431494f4b3012bb5b4f1495086b2076
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-04-22 20:37:07 +0700

    Time: 85 ms (51.30%), Space: 37.7 MB (92.17%) - LeetHub

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cs
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public int GetDecimalValue(ListNode head) 
+    {
+        int sum = 0;
+        int i = 0;
+        
+        ListNode prev = null;
+        
+        while (head != null)
+        {
+            var next = head.next; 
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        
+        while (prev != null)
+        {
+            sum += prev.val * (int)Math.Pow(2, i);
+            i++;
+            prev = prev.next;
+            Console.Write(sum + ",");
+        }
+        return sum;
+    }
+}
