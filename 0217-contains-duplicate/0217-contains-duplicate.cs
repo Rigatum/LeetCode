1d33ddf899d6cdea0ae2f7baea377ef100d6ee64
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-07-02 09:33:15 +0700

    Time: 215 ms (42.24%), Space: 53.6 MB (57.44%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -2,16 +2,6 @@ public class Solution
 {
     public bool ContainsDuplicate(int[] nums) 
     {
-        HashSet<int> hs = new();
-        
-        foreach (int item in nums)
-        {
-            if (hs.Contains(item))
-            {
-                return true;
-            }
-            hs.Add(item);
-        }
-        return false;
+        return new HashSet<int>(nums).Count < nums.Length;
     }
 }
