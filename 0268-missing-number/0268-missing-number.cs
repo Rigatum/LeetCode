3cab589f48128068b218f4b52893db446388b44e
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-07-04 05:29:11 +0700

    Time: 116 ms (22.46%), Space: 43.9 MB (82.65%) - LeetHub

--- 0268-missing-number/0268-missing-number.cs
+++ 0268-missing-number/0268-missing-number.cs
@@ -3,12 +3,15 @@ public class Solution
     public int MissingNumber(int[] nums) 
     {
         int missingNumber = nums.Length;
+        int sum1 = nums.Length;
+        int sum2 = 0;
         
         for (int i = 0; i < nums.Length; i++)
         {
-            missingNumber ^= i ^ nums[i];
+            sum1 += i;
+            sum2 += nums[i];
         }
         
-        return missingNumber;
+        return sum1 - sum2;
     }
 }
