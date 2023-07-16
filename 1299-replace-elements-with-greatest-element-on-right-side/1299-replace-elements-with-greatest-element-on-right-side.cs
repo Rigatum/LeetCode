8246ce887e3fb62edd4a9681445e491a0e3845eb
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-07-16 16:09:51 +0700

    Time: 250 ms (94.62%), Space: 62.8 MB (61.56%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cs
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cs
@@ -2,22 +2,16 @@ public class Solution
 {
     public int[] ReplaceElements(int[] arr)
     {
-        for (int i = 0; i < arr.Length - 1; i++)
-        {
-            int max = 0;
-            
-            for (int j = i + 1; j < arr.Length; j++)
-            {
-                if (arr[j] > max)
-                    max = arr[j];
-            }
-            
-            arr[i] = max;
-        }
-        
-        if (arr.Length > 1)
-            arr[arr.Length - 2] = arr[arr.Length - 1];
+        int max = arr[arr.Length - 1];
         arr[arr.Length - 1] = -1;
+        
+        for (int i = arr.Length - 2; i >= 0; i--)
+        {
+            int temp = max;
+            if (arr[i] > max)
+                max = arr[i];
+            arr[i] = temp;
+        }
         
         return arr;
     }
