97055e04c7dfae2f75fa3eb47f1fa712e3604ff8
Rigatum <73613540+Rigatum@users.noreply.github.com>
2023-07-16 17:37:44 +0700

    Time: 67 ms (54.75%), Space: 37 MB (23.17%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cs
+++ 0058-length-of-last-word/0058-length-of-last-word.cs
@@ -2,16 +2,8 @@ public class Solution
 {
     public int LengthOfLastWord(string s) 
     {
-        s = s.Trim();
-        if (s.Length == 1 && Char.IsLetter(s[0]))
-            return 1;
-        int i = 0;
-        
-        while (s.Length != i && Char.IsLetter(s[s.Length - i - 1]))
-        {
-            i++;
-        }
-        
-        return i;
+        s = s.TrimEnd();
+        string[] ans = s.Split(' ');
+        return ans[ans.Length -1].Length;
     }
 }
